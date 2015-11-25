40995c9e32d5d88cf970554881e79f39acc3fc99
Debarghya Das <dd367@cornell.edu>
2015-11-25 09:57:39 -0500

    final updates

--- all/make_table.sql
+++ all/make_table.sql
@@ -1,8 +1,7 @@
 
 CREATE TABLE allgrad (rowid INTEGER PRIMARY KEY, uni_name TEXT, major TEXT, degree TEXT(5), season TEXT(3), decision TEXT(15), decision_method TEXT(15), decision_date TEXT(10), decision_timestamp INTEGER, ugrad_gpa FLOAT, gre_verbal INTEGER, gre_quant INTEGER, gre_writing FLOAT, is_new_gre INTEGER, gre_subject INTEGER, status TEXT(28), post_date TEXT(10), post_timestamp INTEGER, comments BLOB);
 .mode csv
-# remember to delete header row of CSV
-.import all.csv allgrad
+.import all_clean.csv allgrad
 update allgrad set decision = NULL where decision="";
 update allgrad set decision_method = NULL where decision_method="";
 update allgrad set decision_date = NULL where decision_date="";
--- cs/make_table.sql
+++ cs/make_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE computer (rowid INTEGER PRIMARY KEY, uni_name TEXT, major TEXT, degree TEXT(5), season TEXT(3), decision TEXT(15), decision_method TEXT(15), decision_date TEXT(10), decision_timestamp INTEGER, ugrad_gpa FLOAT, gre_verbal INTEGER, gre_quant INTEGER, gre_writing FLOAT, is_new_gre INTEGER, gre_subject INTEGER, status TEXT(28), post_date TEXT(10), post_timestamp INTEGER, comments BLOB);
 
 .mode csv
-# remember to delete header row of CSV
-.import cs.csv computer
+.import cs_clean.csv computer
 
 
 update computer set decision = NULL where decision="";
